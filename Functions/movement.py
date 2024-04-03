6fa352be2f48a2d5c183dc1d70b876c487a4f88c
Kyle Nurse <114833714+kylenurse@users.noreply.github.com>
2024-04-03 01:22:19 +0000

    Numerous fixes

--- Functions/movement.py
+++ Functions/movement.py
@@ -1,7 +1,11 @@
 from Functions.locationFunctions import get_location
 
-def move_player(command, current_location, locations):
-    if command in current_location.get('exits', {}):
+def move_player(command, current_location, locations, inventory):
+    if  command == "north" and current_location=="church-entrance" and "basement_key" not in inventory:
+        print("The door is locked.")
+        return get_location(current_location,locations), False
+    
+    elif command in current_location.get('exits', {}):
         new_location_id = current_location['exits'][command]
         return get_location(new_location_id, locations), True
     else:
